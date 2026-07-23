9ada6186f078b6948e09b92a926edc9e0e61bfcb
hyird <181540813+hyird@users.noreply.github.com>
2026-07-23 09:40:26 +0800

    Move OpenWrt edgenode sources to package feed

--- edgenode/openwrt/src/edge_capability.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-
-#include <stdbool.h>
-
-bool edge_capability_has_ttyd(void);
--- edgenode/openwrt/src/edge_process.h
+++
@@ -1,4 +0,0 @@
-#pragma once
-
-/* Runs argv without a shell. Negative file descriptors keep stdin/stdout unchanged. */
-int edge_process_run(const char *const argv[], int stdin_fd, int stdout_fd);
